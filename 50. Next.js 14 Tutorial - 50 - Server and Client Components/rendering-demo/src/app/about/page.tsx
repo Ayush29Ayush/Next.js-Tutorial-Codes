84794639737d0e3a7efeb35f73e8b5c3e3f7d6fc
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-11-13 18:13:02 +0530

    feat: Implemented a client and server side component

---
+++ 50. Next.js 14 Tutorial - 50 - Server and Client Components/rendering-demo/src/app/about/page.tsx
@@ -0,0 +1,4 @@
+export default function AboutPage() {
+    console.log("About Page Server Component"); // This will show this message in the code editors terminal, which will signify that this is a server component
+    return <h1>About Page</h1>
+}
